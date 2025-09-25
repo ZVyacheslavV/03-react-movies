5fee8b0867844d15bc1a5fdf563f0433f5c35d14
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-09-25 14:40:06 +0300

    add several components and notofications

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useState /* , useEffect */ } from 'react';
 import { fetchMovies } from '../../services/movieService';
+import SearchBar from '../SearchBar/SearchBar';
+import type { Movie } from '../../types/movie';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import { Toaster } from 'react-hot-toast';
+import { notifyEmpty } from '../../services/notifications';
 
 export default function App() {
-  const [clicks, setClicks] = useState(0);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSearch = async (topic: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+
+      const data = await fetchMovies(topic);
+      if (data.length === 0) notifyEmpty();
+
+      setMovies(data);
+    } catch {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  };
+  /*   const [clicks, setClicks] = useState(0);
 
   useEffect(() => {
     console.log('You can see me only once!');
 
     const handleSearch = async () => console.log(await fetchMovies('dog'));
     handleSearch();
-  }, [clicks]);
+  }, [clicks]); */
 
   return (
-    <button onClick={() => setClicks(clicks + 1)}>
-      You clicked {clicks} times
-    </button>
+    <>
+      <Toaster position="top-center" reverseOrder={true} />
+      <SearchBar onSubmit={handleSearch} />
+      {/* movies.length > 0 && */ <MovieGrid />}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+    </>
   );
+  /*     <button onClick={() => setClicks(clicks + 1)}>
+      You clicked {clicks} times
+    </button> */
 }
